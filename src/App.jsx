86853f5f6eb30636dd769a0edf22e91c186c2ef2
Anastasia Moiseeva <moiseevaanastasia108@gmail.com>
2025-06-16 12:53:49 +0300

    rewrote App.jsx using javascript

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 //декларативный
+import { createElement } from 'react';
 import logo from './assets/logo.svg';
 import './App.css';
 
@@ -7,22 +8,24 @@ export const App = () => {
 	const year = new Date().getFullYear();
 	return (
 		//декларативный
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<div>{year}</div>
-			</header>
-		</div>
+		createElement(
+			'div',
+			{ className: 'App' },
+			createElement('header', {className: 'App-header'},
+				createElement('img', {src: logo, className: 'App-logo', alt: 'logo'}),
+				createElement('p', null,
+					'Edit ',
+					createElement('code', null, 'src/App.js'),
+					' and save to reload.'),
+				createElement('a', {
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+					'Learn React'),
+				createElement('div', null, year),
+			)
+		)
 	);
 };
